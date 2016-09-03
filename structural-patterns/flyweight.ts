fc25d4f0e92252053a9ff3d6e338d9f8a4aa11ed
Arthur Osipenko <arthur.osipenko@gmail.com>
2016-09-04 00:10:39 +0300

    add flyweight

---
+++ structural-patterns/flyweight.ts
@@ -0,0 +1,53 @@
+module FlyweightModule {
+  export class FlyweightFactory {
+    private flyweights: Map<any, any> = new Map();
+
+    constructor() {
+      this.flyweights.set("X", new ConcreteFlyweight());
+      this.flyweights.set("Y", new ConcreteFlyweight());
+      this.flyweights.set("Z", new ConcreteFlyweight());
+    }
+
+    public getFlyweight(key: string): Flyweight {
+      return this.flyweights.get(key);
+    }
+  }
+
+  export abstract class Flyweight {
+    public abstract operation(extrinsicstate: number);
+  }
+
+  export class ConcreteFlyweight implements Flyweight {
+    public operation(extrinsicstate: number) {
+      console.log("ConcreteFlyweight: " + extrinsicstate);
+    }
+  }
+
+  export class UnsharedConcreteFlyweight implements Flyweight {
+    public operation(extrinsicstate: number) {
+      console.log("UnsharedConcreteFlyweight: " + extrinsicstate);
+    }
+  }
+}
+
+//*********************************************
+(function run() {
+  // Arbitrary extrinsic state
+  let extrinsicstate = 22;
+
+  let factory: FlyweightModule.FlyweightFactory = new FlyweightModule.FlyweightFactory();
+
+  // Work with different flyweight instances
+  let fx: FlyweightModule.Flyweight = factory.getFlyweight("X");
+  fx.operation(--extrinsicstate);
+
+  let fy: FlyweightModule.Flyweight = factory.getFlyweight("Y");
+  fy.operation(--extrinsicstate);
+
+  let fz: FlyweightModule.Flyweight = factory.getFlyweight("Z");
+  fz.operation(--extrinsicstate);
+
+  let fu: FlyweightModule.UnsharedConcreteFlyweight = new FlyweightModule.UnsharedConcreteFlyweight();
+
+  fu.operation(--extrinsicstate);
+}());
